172652966bdc211c83917d9843f1353524289bd6
Nikita Sokov <n.nosov@installbiz.ru>
2024-03-01 00:35:34 +0700

    поиск пакетов которых нет в одной из веток

--- src/modules/analize_data.py
+++ src/modules/analize_data.py
@@ -8,5 +8,19 @@ class AnalayzData:
 
     @staticmethod
     def get_df(data):
+        """Получить датафрейм"""
         df = pd.DataFrame(data)
         return df
+
+    def get_uniq_packages(self, first_df, second_df):
+        """Получить уникальные пакеты"""
+        unique_architectures = first_df["arch"].unique()
+        missing_packages_by_arch = {}
+        for arch in unique_architectures:
+            # фильтр по архитектуре
+            df1_arch = first_df[first_df["arch"] == arch]
+            df2_arch = second_df[second_df["arch"] == arch]
+            # поиск уникальных значений
+            missing_packages = df1_arch[~df1_arch["name"].isin(df2_arch["name"])]
+            missing_packages_by_arch[arch] = missing_packages
+        return missing_packages_by_arch
