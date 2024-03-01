2463061dae21a9b465b7417ff097603503cedba4
Nikita Sokov <n.nosov@installbiz.ru>
2024-03-01 19:07:58 +0700

    Добавил логи

--- src/main.py
+++ src/main.py
@@ -3,11 +3,14 @@ from modules.altlinux_api import AltLinuxAPI
 import asyncio
 from modules.analize_data import AnalayzData, ShowData
 from pprint import pprint
+from modules.logger import logger
 
 
 async def main(branch_name_1, branch_name_2, save_on_file):
+    logger.info(f"Скачиваю данные для веток {branch_name_1} и {branch_name_2}")
     api = AltLinuxAPI()
     data = await api.export_branch_binary_packages(branch_name_1, branch_name_2)
+    logger.info("Идёт анализ данных")
     # анализ всех данных
     analayz = AnalayzData(
         data[branch_name_1]["packages"], data[branch_name_2]["packages"]
@@ -24,10 +27,12 @@ async def main(branch_name_1, branch_name_2, save_on_file):
     version_new = analayz.version_release_check(
         analayz.first_branch_df, analayz.second_branch_df
     )
+    logger.info("Формирую Json")
     data = ShowData(branch_name_1, branch_name_2).show(
         first_uniq, second_uniq, version_new
     )
     if save_on_file:
+        logger.info(f"Идёт запись данных в {save_on_file}")
         with open(save_on_file, "w") as f:
             f.write(data)
     else:
